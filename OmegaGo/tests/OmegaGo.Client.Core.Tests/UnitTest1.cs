953341128a3158faef94e4fafa6986b3cd65510b
Martin Zikmund <martinzikmund@live.com>
2016-10-06 09:51:17 +0200

    Omega Go - Core project, Core.Tests project, simple passing unit test for build configuration verification

---
+++ OmegaGo/tests/OmegaGo.Client.Core.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OmegaGo.Client.Core.Tests
+{
+    [TestClass]
+    public class BuildDefinitionTests
+    {
+        [TestMethod]
+        public void PassingTest()
+        {
+            Assert.IsTrue( true );
+        }
+    }
+}
