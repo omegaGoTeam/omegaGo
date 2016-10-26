639dfb2de124520bca0937cb1008bfb7c38bc68a
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2016-10-26 21:56:43 +0200

    Implemented IntersectionState enum and added current board representation in GameTreeNode class based on this enum.

---
+++ OmegaGo/src/Shared/Core/OmegaGo.Client.Core/IntersectionState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core
+{
+    /// <summary>
+    /// Describes the current state of an intersection
+    /// </summary>
+    public enum IntersectionState : byte
+    {
+        None,
+        Black,
+        White
+    }
+}
