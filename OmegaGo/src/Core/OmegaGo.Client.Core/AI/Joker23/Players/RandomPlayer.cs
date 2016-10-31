a994f588e09e60e9e9698f54356c0229f58ac0a6
Petr Hudeček <petrhudecek2010@gmail.com>
2016-10-31 10:22:53 +0100

    adding Joker23 random player

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/AI/Joker23/Players/RandomPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace OmegaGo.Core.AI.Joker23 { 
+public class RandomPlayer : JokerPlayer {
+
+    private System.Random rand;
+
+    public RandomPlayer(char color) : base(color) {
+        rand = new System.Random();
+    }
+
+    public override void planMove(JokerGame game) {
+        char[,] board = game.getBoard();
+         List<JokerPoint> list = new List<JokerPoint>();
+
+        for(int i=0; i< game.getHeight(); i++) {
+            for(int j=0; j< game.getWidth(); j++){
+                if(board[i,j] == '*') {
+                    list.Add(new JokerPoint(i, j));
+                }
+            }
+        }
+
+            JokerPoint randomMove = list[rand.Next(list.Count)];
+
+        while(!list.isEmpty()) {
+            if(makeMove(game, randomMove))
+                return;
+            }
+        }
+    }
+}
