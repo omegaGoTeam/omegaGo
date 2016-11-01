67241f817d59b5f7616ee18350a164ff4b347fe1
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2016-11-01 15:38:03 +0100

    Added some controls to UWP Library view

--- OmegaGo/src/UI/OmegaGo.UI/ViewModels/LibraryViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/ViewModels/LibraryViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MvvmCross.Core.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,48 @@ namespace OmegaGo.UI.ViewModels
 {
     public class LibraryViewModel : ViewModelBase
     {
+        private ObservableCollection<string> _gameList;
+        private ObservableCollection<string> _gameSources;
+
+        private int _selectedGameSourceItemIndex;
+
+        private IMvxCommand _loadCommand;
+        private IMvxCommand _loadFolderCommand;
+        private IMvxCommand _deleteSelectionCommand;
+
+        public ObservableCollection<string> GameList
+        {
+            get { return _gameList; }
+        }
+
+        public ObservableCollection<string> GameSource
+        {
+            get { return _gameSources; }
+        }
+
+        public int SelectedGameSourceItemIndex
+        {
+            get { return _selectedGameSourceItemIndex; }
+            set { SetProperty(ref _selectedGameSourceItemIndex, value); }
+        }
+
+        public IMvxCommand LoadCommand => _loadCommand ?? (_loadCommand = new MvxCommand(() => { }));
+        public IMvxCommand LoadFolderCommand => _loadFolderCommand ?? (_loadFolderCommand = new MvxCommand(() => { }));
+        public IMvxCommand DeleteSelectionCommand => _deleteSelectionCommand ?? (_deleteSelectionCommand = new MvxCommand(() => { }));
+
+        public LibraryViewModel()
+        {
+            _gameList = new ObservableCollection<string>()
+            {
+                "Progame", "Teaching game", "IGS Game #23"
+            };
+
+            _gameSources = new ObservableCollection<string>()
+            {
+                "Preinstalled", "Loaded", "IGS", "All", "Saved"
+            };
+
+            _selectedGameSourceItemIndex = 0;
+        }
     }
 }
