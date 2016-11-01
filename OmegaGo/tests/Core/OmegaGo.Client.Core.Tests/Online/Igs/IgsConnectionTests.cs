78254d3428707d1764c287b5c8634b29f750291a
Martin Zikmund <martinzikmund@live.com>
2016-11-01 14:49:04 +0100

    Fixed unit tests because Tasks

--- OmegaGo/tests/Core/OmegaGo.Client.Core.Tests/Online/Igs/IgsConnectionTests.cs
+++ OmegaGo/tests/Core/OmegaGo.Client.Core.Tests/Online/Igs/IgsConnectionTests.cs
@@ -7,20 +7,20 @@ namespace OmegaGo.Client.Core.Tests.Online.Igs
     [TestClass]
     public class IgsConnectionTests
     {
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]        
         public void LoginThrowsWhenUserNameIsNull()
         {
             IgsConnection connection = new IgsConnection();
-            connection.Login(null, "1234");
+            var task = connection.Login(null, "1234");
+            Assert.IsTrue(task.Exception.InnerException is ArgumentNullException);
         }
 
-        [TestMethod]
-        [ExpectedException( typeof( ArgumentNullException ) )]
+        [TestMethod]        
         public void LoginThrowsWhenPasswordIsNull()
         {
             IgsConnection connection = new IgsConnection();
-            connection.Login( "User", null );
+            var task = connection.Login("User", null);
+            Assert.IsTrue(task.Exception.InnerException is ArgumentNullException);
         }
     }
 }
