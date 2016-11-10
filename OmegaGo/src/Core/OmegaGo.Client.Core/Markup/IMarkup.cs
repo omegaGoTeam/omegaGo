aed29d99907bbebe517fc4f81150e33301122e02
Vítězslav Imrýšek <vitai@tiscali.cz>
2016-11-10 16:40:12 +0100

    Implemented classes describing board markup (in compliance with SGF file format).

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Markup/IMarkup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Markup
+{
+    public interface IMarkup
+    {
+        MarkupKind Kind { get; }
+    }
+}
---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Markup/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Markup
+{
+    public sealed class Line : IMarkup
+    {
+        private Position _from;
+        private Position _to;
+
+        public MarkupKind Kind => MarkupKind.Line;
+        public Position From => _from;
+        public Position To => _to;
+
+        public Line(Position from, Position to)
+        {
+            _from = from;
+            _to = to;
+        }
+    }
+}
