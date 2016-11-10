e11e39944eec1f5a109eac89b0796c557d8722b4
Martin Zikmund <martinzikmund@live.com>
2016-11-10 17:24:27 +0100

    SGF parser rewrite started

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/SgfRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf
+{
+    internal class SgfRoot
+    {
+        public List<SgfProperty> GlobalCommands { get; } = new List<SgfProperty>();
+        public List<SgfGameTree> Games { get; } = new List<SgfGameTree>();
+    }
+}
