8755b6f0623fa9994dbdc4ec9587ab2bb5ec80bd
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2016-11-10 14:43:53 +0100

    Board customization in Game View

---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/BoardSettingsControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using OmegaGo.UI.WindowsUniversal.Services.Game;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OmegaGo.UI.WindowsUniversal.UserControls
+{
+    public sealed partial class BoardSettingsControl : UserControl
+    {
+        public static readonly DependencyProperty BoardDataProperty = 
+                DependencyProperty.Register(
+                        "BoardData", 
+                        typeof(BoardData), 
+                        typeof(BoardSettingsControl), 
+                        new PropertyMetadata(null));
+
+        public BoardData BoardData
+        {
+            get { return (BoardData)GetValue(BoardDataProperty); }
+            set { SetValue(BoardDataProperty, value); }
+        }
+
+        public BoardSettingsControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
