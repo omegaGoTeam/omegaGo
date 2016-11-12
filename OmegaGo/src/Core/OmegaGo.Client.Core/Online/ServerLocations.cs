38c7ab3246bb0e2778d6a42fa8d524cd59c5af2a
Petr Hudeček <petrhudecek2010@gmail.com>
2016-11-12 02:10:45 +0100

    moving files; minor documentation

--- OmegaGo/src/Core/OmegaGo.Client.Core/Online/ServerLocations.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Online/ServerLocations.cs
@@ -8,8 +8,10 @@ namespace OmegaGo.Core.Online
 {
     class ServerLocations
     {
+        // The following two hosts are identical.
         public const string IgsPrimary = "igs.joyjoy.net";
         public const string IgsSecondary = "210.155.158.200";
+        // The IGS server runs at these ports: they all redirect to the same server application
         public const int IgsPortPrimary = 6969;
         public const int IgsPortSecondary = 7777;
         public const int IgsPortTertiary = 28155;
