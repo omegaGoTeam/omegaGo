dfabf80a8cea122415e39bf11eab4a0eee8d0bdf
Martin Zikmund <martinzikmund@sphereline.com>
2016-11-13 11:39:56 +0100

    SGF parsing result class
    Related Work Items: #158

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/SgfParseResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf
+{
+    /// <summary>
+    /// Represents the result of SGF parsing
+    /// Informs the caller about possible warnings during the parsing
+    /// </summary>
+    internal class SgfParseResult
+    {
+        /// <summary>
+        /// Creates the parse result
+        /// </summary>
+        /// <param name="status">Status of parsing</param>
+        /// <param name="root">Root of the parsed SGF tree</param>
+        public SgfParseResult(SgfParseStatus status, SgfRoot root = null)
+        {
+            if (root == null && status != SgfParseStatus.Failed) throw new ArgumentNullException(nameof(root), "Root has to be supplied when parsing did not fail");
+            Status = status;
+            Root = root;
+        }
+
+        /// <summary>
+        /// Status of the parsing
+        /// </summary>
+        public SgfParseStatus Status { get; }
+
+        /// <summary>
+        /// Root of the parsed SGF tree
+        /// </summary>
+        public SgfRoot Root { get; }
+    }
+}
