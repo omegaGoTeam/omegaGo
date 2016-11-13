fb333834b77aba18fae87b9b823747e8b69336ce
Martin Zikmund <martinzikmund@sphereline.com>
2016-11-13 11:41:07 +0100

    SGF parse status
    Related Work Items: #158

--- OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/SgfParseStatus.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/SgfParseStatus.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace OmegaGo.Core.Sgf
 {
+    /// <summary>
+    /// Status of SGF parsing
+    /// </summary>
     public enum SgfParseStatus
     {
         Success,
