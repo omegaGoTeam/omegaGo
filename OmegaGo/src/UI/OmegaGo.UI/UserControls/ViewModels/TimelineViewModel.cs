2530484029778b85d131c1e6775a60eb3a75f715
Vítězslav Imrýšek <vitai@tiscali.cz>
2016-11-15 19:53:48 +0100

    Added Timeline control for UWP + modified Game View to use it

---
+++ OmegaGo/src/UI/OmegaGo.UI/UserControls/ViewModels/TimelineViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MvvmCross.Core.ViewModels;
+using OmegaGo.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.UI.UserControls.ViewModels
+{
+    public sealed class TimelineViewModel : MvxViewModel
+    {
+        private GameTree _gameTree;
+
+        private GameTreeNode _timelineNode;
+
+        public GameTree GameTree
+        {
+            get { return _gameTree; }
+            set { SetProperty(ref _gameTree, value); }
+        }
+
+        public GameTreeNode TimelineNode
+        {
+            get { return _timelineNode; }
+            set { SetProperty(ref _timelineNode, value); }
+        }
+
+        public TimelineViewModel()
+        {
+
+        }
+    }
+}
---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/TimelineControl.xaml.cs
@@ -0,0 +1,160 @@
+﻿using OmegaGo.Core;
+using OmegaGo.UI.UserControls.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OmegaGo.UI.WindowsUniversal.UserControls
+{
+    public sealed partial class TimelineControl : UserControl
+    {
+        public static readonly DependencyProperty ViewModelProperty =
+                DependencyProperty.Register(
+                        "ViewModel",
+                        typeof(TimelineViewModel),
+                        typeof(TimelineControl),
+                        new PropertyMetadata(null, TimelineChanged));
+
+        public static readonly DependencyProperty TimelineNodeSizeProperty = 
+                DependencyProperty.Register(
+                        "TimelineNodeSize", 
+                        typeof(int), 
+                        typeof(TimelineControl), 
+                        new PropertyMetadata(32, TimelineChanged));
+        
+        public static readonly DependencyProperty TimelineNodeSpacingProperty =
+                DependencyProperty.Register(
+                        "TimelineNodeSpacing",
+                        typeof(int),
+                        typeof(TimelineControl),
+                        new PropertyMetadata(8, TimelineChanged));
+
+        private static readonly DependencyProperty TimelineDepthProperty =
+                DependencyProperty.Register(
+                        "TimelineDepth",
+                        typeof(int),
+                        typeof(TimelineControl),
+                        new PropertyMetadata(0));
+
+        private static void TimelineChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TimelineControl timeline = d as TimelineControl;
+
+            if (timeline != null)
+            {
+                timeline.Draw();
+            }
+        }
+
+        public TimelineViewModel ViewModel
+        {
+            get { return (TimelineViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public int TimelineNodeSize
+        {
+            get { return (int)GetValue(TimelineNodeSizeProperty); }
+            set { SetValue(TimelineNodeSizeProperty, value); }
+        }
+
+        public int TimelineNodeSpacing
+        {
+            get { return (int)GetValue(TimelineNodeSpacingProperty); }
+            set { SetValue(TimelineNodeSpacingProperty, value); }
+        }
+
+        public int TimelineDepth
+        {
+            get { return (int)GetValue(TimelineDepthProperty); }
+            private set { SetValue(TimelineDepthProperty, value); }
+        }
+
+        public TimelineControl()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void Draw()
+        {
+            TimelineDepth = 0;
+            canvas.Children.Clear();
+            canvas.Width = 0;
+            canvas.Height = 0;
+
+            if (ViewModel?.GameTree != null)
+            {
+                int requiredHeight = DrawNode(ViewModel.GameTree.GameTreeRoot, 0, 0, 0) + 1;
+
+                canvas.Height =
+                    requiredHeight * TimelineNodeSize +
+                    requiredHeight * TimelineNodeSpacing;
+
+                canvas.Width =
+                    (TimelineDepth + 1) * TimelineNodeSize +
+                    (TimelineDepth + 1) * TimelineNodeSpacing;
+            }
+        }
+
+        private int DrawNode(GameTreeNode node, int depth, int offset, int parentOffset)
+        {
+            int nodeOffset = offset;
+            TimelineDepth = Math.Max(TimelineDepth, depth);
+
+            Ellipse nodeVisual = new Ellipse();
+            nodeVisual.Width = TimelineNodeSize;
+            nodeVisual.Height = TimelineNodeSize;
+            
+            nodeVisual.StrokeThickness = 2;
+            nodeVisual.Tag = node;
+
+            if (node.Move.WhoMoves == StoneColor.Black)
+                nodeVisual.Fill = new SolidColorBrush(Colors.Black);
+            else if (node.Move.WhoMoves == StoneColor.White)
+                nodeVisual.Fill = new SolidColorBrush(Colors.White);
+
+            nodeVisual.PointerEntered += (s, e) => nodeVisual.Stroke = new SolidColorBrush(Colors.Yellow);
+            nodeVisual.PointerExited += (s, e) => nodeVisual.Stroke = null;
+
+            Canvas.SetTop(nodeVisual, offset * TimelineNodeSize + offset * TimelineNodeSpacing);
+            Canvas.SetLeft(nodeVisual, depth * TimelineNodeSize + depth * TimelineNodeSpacing);
+
+            Line nodeParentLine = new Line();
+            nodeParentLine.Stroke = new SolidColorBrush(Colors.Gray);
+            nodeParentLine.StrokeThickness = 1;
+            nodeParentLine.X1 = (depth) * TimelineNodeSize + (depth - 1) * TimelineNodeSpacing;
+            nodeParentLine.Y1 = (parentOffset) * TimelineNodeSize + parentOffset * TimelineNodeSpacing + TimelineNodeSize * 0.5;
+            nodeParentLine.X2 = depth * TimelineNodeSize + depth * TimelineNodeSpacing;
+            nodeParentLine.Y2 = offset * TimelineNodeSize + offset * TimelineNodeSpacing + TimelineNodeSize * 0.5;
+
+            canvas.Children.Add(nodeParentLine);
+            canvas.Children.Add(nodeVisual);
+            
+            foreach(GameTreeNode childNode in node.Branches)
+            {
+                offset = DrawNode(childNode, depth + 1, offset, nodeOffset);
+                offset++;
+            }
+
+            if (node.Branches.Count > 0)
+                offset--;
+
+            return offset;
+        }
+    }
+}
