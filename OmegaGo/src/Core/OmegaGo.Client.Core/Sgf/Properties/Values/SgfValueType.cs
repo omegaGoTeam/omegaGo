65ad2a7fbf747ce97f032e4af8a2c1af4361b7f5
Martin Zikmund <martinzikmund@live.com>
2016-11-23 14:54:38 +0100

    Sgf values

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfValueType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf.Properties.Values
+{
+    internal enum SgfValueType
+    {
+        Double,
+        Point,
+        Text,
+        SimpleText,
+        Unknown
+    }
+}
