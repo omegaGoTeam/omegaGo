9525121375ce2ac059d1c4ff0137c67557e38380
Petr Hudeček <petrhudecek2010@gmail.com>
2016-11-25 23:31:04 +0100

    User can enter and exit fullscreen mode via the common keyboard hotkey.
    Related Work Items: #218

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/App.xaml.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/App.xaml.cs
@@ -96,11 +96,12 @@ namespace OmegaGo.UI.WindowsUniversal
                 // Ensure the current window is active
                 SetupTitleBar();
                 Window.Current.Activate();
-                Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
-                Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+                Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += Dispatcher_AcceleratorKeyActivated;
                 await InitializeMvvmCrossAsync();
             }
         }
+
+     
 
         /// <summary>
         /// Setup language        
@@ -157,39 +158,28 @@ namespace OmegaGo.UI.WindowsUniversal
                 statusBar.ForegroundColor = Colors.Black;
             }
         }
-
-        private bool altIsHeld = false;
-
-        private void CoreWindow_KeyUp( Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args )
+        /// <summary>
+        /// This method uses parts of http://stackoverflow.com/a/39929182/1580088.
+        /// This event is called whenever a key is pressed down on a computer keyboard. We use this method
+        /// to trigger application-wide hotkeys, most notably the Alt+Enter combination for
+        /// toggling fullscreen mode.
+        /// </summary>
+        private void Dispatcher_AcceleratorKeyActivated(Windows.UI.Core.CoreDispatcher sender, Windows.UI.Core.AcceleratorKeyEventArgs args)
         {
-            if ( args.VirtualKey == Windows.System.VirtualKey.Control )
+            // "menu key" is Alt, in Microsoft-speak.
+            if (args.VirtualKey == Windows.System.VirtualKey.Enter && args.KeyStatus.IsMenuKeyDown)
             {
-                // Yes, it should be Alt+Enter, not Ctrl+Enter, but "Alt" is not yet working.
-                altIsHeld = false;
-            }
-            if ( args.VirtualKey == Windows.System.VirtualKey.Enter )
-            {
-                if ( altIsHeld )
+                if (ApplicationView.GetForCurrentView().IsFullScreenMode)
                 {
-                    if ( ApplicationView.GetForCurrentView().IsFullScreenMode )
-                    {
-                        ApplicationView.GetForCurrentView().ExitFullScreenMode();
-                    }
-                    else
-                    {
-                        ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
-                    }
+                    ApplicationView.GetForCurrentView().ExitFullScreenMode();
+                }
+                else
+                {
+                    ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
                 }
             }
         }
-
-        private void CoreWindow_KeyDown( Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args )
-        {
-            if ( args.VirtualKey == Windows.System.VirtualKey.Control )
-            {
-                altIsHeld = true;
-            }
-        }
+      
 
         private async Task InitializeMvvmCrossAsync()
         {
