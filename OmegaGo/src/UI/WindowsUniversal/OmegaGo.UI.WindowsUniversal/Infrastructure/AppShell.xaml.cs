884caf2d996100cf7fc54a7bddafd35c00f464ac
Martin Zikmund <martinzikmund@live.com>
2016-11-29 23:13:10 +0100

    Simple app shell page
    Related Work Items: #248

---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Infrastructure/AppShell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OmegaGo.UI.WindowsUniversal.Infrastructure
+{
+    /// <summary>
+    /// The Shell around the whole app
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public AppShell()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Custom app title bar
+        /// </summary>
+        public FrameworkElement AppTitleBar => TitleBar;
+
+        /// <summary>
+        /// Main frame that hosts app views
+        /// </summary>
+        public Frame MainFrame => Frame;
+    }
+}
