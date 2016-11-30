e45f4cf171ffee783f615599c1b3a6aa67954421
Vítězslav Imrýšek <vitai@tiscali.cz>
2016-11-30 20:24:02 +0100

    Start work on Multiplayer dashboard + reference Sockets PCL for UWP

--- OmegaGo/src/UI/OmegaGo.UI/ViewModels/MultiplayerDashboardViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/ViewModels/MultiplayerDashboardViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MvvmCross.Core.ViewModels;
+using OmegaGo.Core;
+using OmegaGo.Core.Online.Igs;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,57 @@ namespace OmegaGo.UI.ViewModels
 {
     public class MultiplayerDashboardViewModel : ViewModelBase
     {
+        private IgsConnection _igsConnection;
+
+        private ObservableCollection<Game> _onlineGames;
+        private ObservableCollection<IgsUser> _onlinePlayers;
+
+        private MvxCommand _refreshOnlineGamesCommand;
+        private MvxCommand _refreshOnlinePlayersCommand;
+
+        public ObservableCollection<Game> OnlineGames => _onlineGames;
+        public ObservableCollection<IgsUser> OnlinePlayers => _onlinePlayers;
+
+        public MvxCommand RefreshOnlineGamesCommand => _refreshOnlineGamesCommand ?? (_refreshOnlineGamesCommand = new MvxCommand(() => RefreshOnlineGames()));
+        public MvxCommand RefreshOnlinePlayersCommand => _refreshOnlinePlayersCommand ?? (_refreshOnlinePlayersCommand = new MvxCommand(() => RefreshOnlinePlayers()));
+
+
+        public MultiplayerDashboardViewModel()
+        {
+            _igsConnection = new IgsConnection();
+
+            _onlineGames = new ObservableCollection<Game>();
+            _onlinePlayers = new ObservableCollection<IgsUser>();
+
+            _igsConnection.Connect();
+        }
+
+        private async void RefreshOnlineGames()
+        {
+            OnlineGames.Clear();
+            var gamesInProgress = await _igsConnection.ListGamesInProgress();
+
+            foreach (var onlineGame in gamesInProgress)
+            {
+                OnlineGames.Add(onlineGame);
+            }
+        }
+
+        private async void RefreshOnlinePlayers()
+        {
+            OnlinePlayers.Clear();
+            var onlinePlayers = await _igsConnection.ListOnlinePlayers();
+
+            foreach (var onlinePlayer in onlinePlayers)
+            {
+                OnlinePlayers.Add(onlinePlayer);
+            }
+        }
+
+        protected override void GoBack()
+        {
+            _igsConnection.Disconnect();
+            base.GoBack();
+        }
     }
 }
