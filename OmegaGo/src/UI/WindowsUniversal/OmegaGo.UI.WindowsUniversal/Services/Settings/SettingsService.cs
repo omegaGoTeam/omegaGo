6a69be807744258a2fa7867162e6fd1f592f13f3
Martin Zikmund <martinzikmund@live.com>
2016-12-03 21:23:32 +0100

    Documentation of SettingsGroup and SettingsService
    Related Work Items: #260, #261

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Services/Settings/SettingsService.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Services/Settings/SettingsService.cs
@@ -8,6 +8,9 @@ using Windows.Storage;
 
 namespace OmegaGo.UI.WindowsUniversal.Services.Settings
 {
+    /// <summary>
+    /// UWP implementation of settings service using <see cref="ApplicationData">ApplicationData</see>
+    /// </summary>
     internal class SettingsService : SettingsServiceBase
     {
         public override T GetSetting<T>(string key, Func<T> defaultValueBuilder, SettingLocality locality = SettingLocality.Local)
@@ -22,7 +25,7 @@ namespace OmegaGo.UI.WindowsUniversal.Services.Settings
                 }
                 catch
                 {
-                    //invalid value, remove
+                    //invalid value for the given typ, remove
                     container.Values.Remove(key);
                 }
             }
@@ -30,14 +33,14 @@ namespace OmegaGo.UI.WindowsUniversal.Services.Settings
         }
 
         public override void SetSetting<T>(string key, T value, SettingLocality locality = SettingLocality.Local)
-        {
+        {            
             var container = locality == SettingLocality.Roamed ? ApplicationData.Current.RoamingSettings : ApplicationData.Current.LocalSettings;
             if (container.Values.ContainsKey(key))
             {
                 container.Values[key] = value;
             }
             else
-            {
+            {               
                 container.Values.Add(key, value);
             }
         }
