03ddeb998896ec932d1214eb40b9bf4366cf4547
Martin Zikmund <martinzikmund@live.com>
2016-12-04 13:07:50 +0100

    SGF point parsing and serialization
    Related Work Items: #266, #267, #268

--- OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfPoint.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfPoint.cs
@@ -9,7 +9,7 @@ namespace OmegaGo.Core.Sgf.Properties.Values
     /// <summary>
     /// SGF point
     /// </summary>
-    public struct SgfPoint : IComparable<SgfPoint>
+    public struct SgfPoint : IEquatable<SgfPoint>, IComparable<SgfPoint>
     {
         /// <summary>
         /// Creates a SGF Point
@@ -18,8 +18,68 @@ namespace OmegaGo.Core.Sgf.Properties.Values
         /// <param name="row">Row</param>
         public SgfPoint(int column, int row)
         {
+            if (column < -1 || column > 52) throw new ArgumentOutOfRangeException(nameof(column));
+            if (row < -1 || row > 52) throw new ArgumentOutOfRangeException(nameof(row));
             Column = column;
             Row = row;
+        }
+
+        /// <summary>
+        /// Parses a SGF point from a property value
+        /// </summary>
+        /// <param name="value">Property value</param>
+        /// <returns>SGF point</returns>
+        public static SgfPoint Parse(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length != 2)
+                throw new SgfParseException($"Invalid SGF point value: '{value}'");
+            var columnChar = value[0];
+            var rowChar = value[1];
+            return new SgfPoint(
+                    PointCharToValue(columnChar),
+                    PointCharToValue(rowChar)
+                );
+        }
+
+        /// <summary>
+        /// Pass move
+        /// </summary>
+        public static SgfPoint Pass { get; } = new SgfPoint(-1, -1);
+
+        /// <summary>
+        /// Converts a SGF based char to internal coordinate value
+        /// </summary>
+        /// <param name="character">Character from SGF file</param>
+        /// <returns>Coordinate value in SGF point structure</returns>
+        private static int PointCharToValue(char character)
+        {
+            var isEnglishLower = (character >= 'a' && character <= 'z');
+            var isEnglishUpper = (character >= 'A' && character <= 'Z');
+
+            if (isEnglishLower)
+            {
+                return character - 'a';
+            }
+            else if (isEnglishUpper)
+            {
+                return character - 'A';
+            }
+            else
+            {
+                //invalid                
+                throw new SgfParseException($"Invalid SGF point char: '{character}'");
+            }
+        }
+
+        /// <summary>
+        /// Value to point char
+        /// </summary>
+        /// <param name="value">Internal struct coordinate value</param>
+        /// <returns>SGF coordinate char</returns>
+        private static char ValueToPointChar(int value)
+        {
+            if (value < 0 || value > 52) throw new ArgumentOutOfRangeException(nameof(value));
+            return (char)(value < 27 ? 'a' + value : 'A' + value);
         }
 
         /// <summary>
@@ -32,31 +92,54 @@ namespace OmegaGo.Core.Sgf.Properties.Values
         /// </summary>
         public int Row { get; }
 
-        public static bool operator <(SgfPoint left, SgfPoint right) => left.CompareTo(right) < 0;
+        /// <summary>
+        /// Checks if the point represents a pass move
+        /// </summary>
+        public bool IsPass => Column == -1;
 
-        public static bool operator <=(SgfPoint left, SgfPoint right) => left.CompareTo(right) <= 0;
+        public static bool operator ==(SgfPoint left, SgfPoint right) => left.Equals(right);
 
-        public static bool operator >(SgfPoint left, SgfPoint right) => left.CompareTo(right) > 0;
-
-        public static bool operator >=(SgfPoint left, SgfPoint right) => left.CompareTo(right) >= 0;
-
+        public static bool operator !=(SgfPoint left, SgfPoint right) => !(left == right);
 
         /// <summary>
-        /// Compares two SGF points
+        /// Checks for SGF point equality
         /// </summary>
-        /// <param name="other">SGF point to compare</param>
-        /// <returns>The ordering of SGF points</returns>
-        public int CompareTo(SgfPoint other)
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(SgfPoint other) => Row == other.Row && Column == other.Column;
+
+        /// <summary>
+        /// Checks for equality
+        /// </summary>
+        /// <param name="obj">Second instance</param>
+        /// <returns>Are instances equal?</returns>
+        public override bool Equals(object obj)
         {
-            if (Column <= other.Column && Row <= other.Row)
+            var secondPoint = obj as SgfPoint?;
+            if (secondPoint != null)
             {
-                if (Column == other.Column && Row == other.Row)
-                {
-                    return 0;
-                }
-                return -1;
+                return Equals(secondPoint.Value);
             }
-            return 1;
+            return false;
+        }
+
+        /// <summary>
+        /// Hash code of the SGF point
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode() => Row * 101 + Column;
+
+        /// <summary>
+        /// String representation of the SGF point
+        /// </summary>
+        /// <returns>SGF value</returns>
+        public override string ToString()
+        {
+            //handle pass move
+            if (IsPass) return string.Empty;
+
+            char columnChar = ValueToPointChar( Column );
+            char rowChar = ValueToPointChar( Row );
         }
     }
 }
---
+++ OmegaGo/tests/Core/OmegaGo.Client.Core.Tests/Sgf/Properties/Values/SgfPointTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmegaGo.Core.Sgf.Properties.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Tests.Sgf.Properties.Values
+{
+    [TestClass]
+    public class SgfPointTests
+    {
+        [TestMethod]
+        public void SgfPointComparisonWorks()
+        {
+            SgfPoint first = new SgfPoint(0, 0);
+            SgfPoint second = new SgfPoint(1, 1);
+        }
+    }
+}
