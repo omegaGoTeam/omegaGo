23f980bf58a64eae7d12f50b3560dd937e3b6414
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2016-12-04 15:04:54 +0100

    Moved MoveKind into its own file and added None member into it.

--- OmegaGo/src/Core/OmegaGo.Client.Core/Move.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Move.cs
@@ -61,19 +61,4 @@ namespace OmegaGo.Core
             throw new Exception("This move kind does not exist.");
         }
     }
-
-    /// <summary>
-    /// There are two kinds of moves - placing a stone; and passing.
-    /// </summary>
-    public enum MoveKind
-    {
-        /// <summary>
-        /// The most common move - a player places a stone on the board.
-        /// </summary>
-        PlaceStone,
-        /// <summary>
-        /// The other move - a player passes to signal that they think the game is over.
-        /// </summary>
-        Pass
-    }
 }
---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/MoveKind.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core
+{
+    /// <summary>
+    /// There are two kinds of moves - placing a stone; and passing.
+    /// </summary>
+    public enum MoveKind
+    {
+        /// <summary>
+        /// Used for SGF, where not every node has a move defined.
+        /// </summary>
+        None,
+        /// <summary>
+        /// The most common move - a player places a stone on the board.
+        /// </summary>
+        PlaceStone,
+        /// <summary>
+        /// The other move - a player passes to signal that they think the game is over.
+        /// </summary>
+        Pass
+    }
+}
