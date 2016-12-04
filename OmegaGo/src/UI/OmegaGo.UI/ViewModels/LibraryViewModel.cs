4f8d4b5230efc1ba488014bcc866a367e39d4a1b
Martin Zikmund <martinzikmund@live.com>
2016-12-04 02:59:20 +0100

    Library file open button
    Related Work Items: #243

--- OmegaGo/src/UI/OmegaGo.UI/ViewModels/LibraryViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/ViewModels/LibraryViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using MvvmCross.Core.ViewModels;
+using OmegaGo.UI.Services.Files;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OmegaGo.UI.ViewModels
 {
     public class LibraryViewModel : ViewModelBase
     {
+        private readonly IFilePickerService _filePicker = null;
+
         private ObservableCollection<string> _gameList;
         private ObservableCollection<string> _gameSources;
 
@@ -18,6 +22,24 @@ namespace OmegaGo.UI.ViewModels
         private IMvxCommand _loadCommand;
         private IMvxCommand _loadFolderCommand;
         private IMvxCommand _deleteSelectionCommand;
+        private ICommand _openFileCommand;
+
+        public LibraryViewModel( IFilePickerService filePicker )
+        {
+            _filePicker = filePicker;
+
+            _gameList = new ObservableCollection<string>()
+            {
+                "Progame", "Teaching game", "IGS Game #23"
+            };
+
+            _gameSources = new ObservableCollection<string>()
+            {
+                "Preinstalled", "Loaded", "IGS", "All", "Saved"
+            };
+
+            _selectedGameSourceItemIndex = 0;
+        }
 
         public ObservableCollection<string> GameList
         {
@@ -38,20 +60,14 @@ namespace OmegaGo.UI.ViewModels
         public IMvxCommand LoadCommand => _loadCommand ?? (_loadCommand = new MvxCommand(() => { }));
         public IMvxCommand LoadFolderCommand => _loadFolderCommand ?? (_loadFolderCommand = new MvxCommand(() => { }));
         public IMvxCommand DeleteSelectionCommand => _deleteSelectionCommand ?? (_deleteSelectionCommand = new MvxCommand(() => { }));
-
-        public LibraryViewModel()
+        public ICommand OpenFileCommand => _openFileCommand ?? (_openFileCommand = new MvxCommand(OpenFile));
+        
+        /// <summary>
+        /// Opening SGF file directly
+        /// </summary>
+        private async void OpenFile()
         {
-            _gameList = new ObservableCollection<string>()
-            {
-                "Progame", "Teaching game", "IGS Game #23"
-            };
-
-            _gameSources = new ObservableCollection<string>()
-            {
-                "Preinstalled", "Loaded", "IGS", "All", "Saved"
-            };
-
-            _selectedGameSourceItemIndex = 0;
+            var fileContents = await _filePicker.PickAndReadFileAsync(".sgf");
         }
     }
 }
