28451dffc4ddd718da86d4549ae939899076f925
Martin Zikmund <martinzikmund@live.com>
2016-12-04 23:19:22 +0100

    Unknown property value
    Related Work Items: #273

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfUnknownPropertyValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf.Properties.Values
+{
+    /// <summary>
+    /// Unknown SGF property value
+    /// </summary>
+    public class SgfUnknownPropertyValue : SgfSimplePropertyValueBase<string>
+    {
+        /// <summary>
+        /// Stores simple string value
+        /// </summary>
+        /// <param name="value">String value</param>
+        public SgfUnknownPropertyValue(string value) : base(value)
+        {            
+        }
+
+        /// <summary>
+        /// Value type
+        /// </summary>
+        public override SgfValueType ValueType => SgfValueType.Unknown;
+
+        /// <summary>
+        /// Serializes the unknown property value
+        /// </summary>
+        /// <returns>Value itself</returns>
+        public override string Serialize() => Value;
+    }
+}
