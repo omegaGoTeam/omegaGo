56d95a309c91bfa58d78eb9a2bc28183aa705f1d
Martin Zikmund <martinzikmund@live.com>
2016-12-04 23:19:42 +0100

    Simple property value base class
    Related Work Items: #272

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/ISgfPropertyValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf.Properties.Values
+{
+    /// <summary>
+    /// SGF property value
+    /// </summary>
+    public interface ISgfPropertyValue
+    {
+        SgfValueType ValueType { get; }
+
+        /// <summary>
+        /// Parses the SGF property value from string
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>Parsed property value</returns>
+        ISgfPropertyValue Parse(string value);
+
+        /// <summary>
+        /// Serializes the SGF property value to string
+        /// </summary>
+        /// <returns>String</returns>
+        string Serialize();
+    }
+}
