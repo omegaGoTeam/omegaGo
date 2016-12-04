0d7c1ffb75b35935e6c37d02a8025066af0f92a2
Martin Zikmund <martinzikmund@live.com>
2016-12-04 23:44:14 +0100

    SGF point rectangle tests
    Related Work Items: #276

---
+++ OmegaGo/tests/Core/OmegaGo.Client.Core.Tests/Sgf/Properties/Values/SgfPointRectangleTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OmegaGo.Core.Sgf.Properties.Values;
+
+namespace OmegaGo.Core.Tests.Sgf.Properties.Values
+{
+    [TestClass]
+    public class SgfPointRectangleTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SgfPointRectangleDoesNotAcceptNonRectangleAreas()
+        {
+            var point = new SgfPointRectangle(new SgfPoint(1, 2), new SgfPoint(0, 0));
+        }
+
+        [TestMethod]
+        public void SinglePointRectangleIsProperlyCreated()
+        {
+            var single = new SgfPoint(12, 2);
+            var rectangle = new SgfPointRectangle(single);
+            Assert.AreEqual(single, rectangle.UpperLeft);
+            Assert.AreEqual(single, rectangle.LowerRight);
+            Assert.AreEqual(1, rectangle.ToList());
+            Assert.AreEqual(single, rectangle.First());
+        }
+    }
+}
