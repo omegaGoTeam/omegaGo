5ff1870aee4249c17639202ee0419739c67b18bc
Martin Zikmund <martinzikmund@live.com>
2016-12-07 09:11:46 +0100

    SGF real value started
    Related Work Items: #290, #291

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfRealValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf.Properties.Values
+{
+    /// <summary>
+    /// SGF real property value
+    /// </summary>
+    public class SgfRealValue : SgfSimplePropertyValueBase<decimal>
+    {
+        /// <summary>
+        /// Creates SGF real property value
+        /// </summary>
+        /// <param name="value"></param>
+        public SgfRealValue( decimal value ) : base( value ) {}
+
+        /// <summary>
+        /// Real SGF value
+        /// </summary>
+        public override SgfValueType ValueType => SgfValueType.Real;
+
+        /// <summary>
+        /// Parses SGF real value
+        /// </summary>
+        /// <param name="value">Value to parse</param>
+        /// <returns>Parsed property value</returns>
+        public static ISgfPropertyValue Parse( string value )
+        {
+            throw new NotImplementedException();   
+        }
+
+        /// <summary>
+        /// Serializes the value
+        /// </summary>
+        /// <returns>SGF serialized real value</returns>
+        public override string Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
