48f6fff20ec5c28b4c2a0f0c03929fb2a837d74f
Martin Zikmund <martinzikmund@live.com>
2016-12-07 12:53:34 +0100

    SGF color value support started
    Related Work Items: #297, #298

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/SgfColorValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmegaGo.Core.Sgf.Properties.Values.ValueTypes;
+
+namespace OmegaGo.Core.Sgf.Properties.Values
+{
+    /// <summary>
+    /// SGF Color property value type
+    /// </summary>
+    public class SgfColorValue : SgfSimplePropertyValueBase<SgfColor>
+    {
+        /// <summary>
+        /// Creates SGF color value
+        /// </summary>
+        /// <param name="value">Color</param>
+        public SgfColorValue( SgfColor value ) : base( value ) {}
+
+        /// <summary>
+        /// Color property value type
+        /// </summary>
+        public override SgfValueType ValueType => SgfValueType.Color;
+
+        /// <summary>
+        /// Parses a SGF Color value
+        /// </summary>
+        /// <param name="value">Value to parse</param>
+        /// <returns>SGF color instance</returns>
+        public static SgfColorValue Parse( string value )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Serializes SGF color instance
+        /// </summary>
+        /// <returns></returns>
+        public override string Serialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Sgf/Properties/Values/ValueTypes/SgfColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Sgf.Properties.Values.ValueTypes
+{
+    /// <summary>
+    /// Colors of players
+    /// </summary>
+    public enum SgfColor
+    {
+        White = 'W',
+        Black = 'B'
+    }
+}
