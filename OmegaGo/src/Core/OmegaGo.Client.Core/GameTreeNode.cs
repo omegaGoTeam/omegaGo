3d5089b29a6f538d67c73032ee8e1caefc6a5b10
Martin Zikmund <martinzikmund@live.com>
2016-12-12 14:29:23 +0100

    Added tsumego file, updated GameTreeNode to initialize lists on construction

--- OmegaGo/src/Core/OmegaGo.Client.Core/GameTreeNode.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/GameTreeNode.cs
@@ -20,8 +20,8 @@ namespace OmegaGo.Core
         public string Comment { get; set; }
         public string Name { get; set; }
 
-        public List<Position> AddBlack { get; set; }
-        public List<Position> AddWhite { get; set; }
+        public List<Position> AddBlack { get; set; } = new List<Position>();
+        public List<Position> AddWhite { get; set; } = new List<Position>();
 
         /// <summary>
         /// Describes current state of the entire game board. Can be null.
