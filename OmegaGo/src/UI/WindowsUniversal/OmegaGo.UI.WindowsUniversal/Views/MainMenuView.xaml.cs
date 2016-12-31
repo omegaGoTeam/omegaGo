8d7ef77fbd5263eac9366ade8e4872e45cc1c11d
Martin Zikmund <martinzikmund@live.com>
2016-12-31 20:26:31 +0100

    Manual binding of languages to the main menu language selector
    Related Work Items: #381, #382

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Views/MainMenuView.xaml.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Views/MainMenuView.xaml.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using OmegaGo.UI.Services.Localization;
 using OmegaGo.UI.ViewModels;
 using OmegaGo.UI.WindowsUniversal.Infrastructure;
 
@@ -17,7 +20,64 @@ namespace OmegaGo.UI.WindowsUniversal.Views
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            
+            LoadLanguageMenu();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            UnloadLanguageMenu();
+        }
+
+        private void UnloadLanguageMenu()
+        {
+            VM.PropertyChanged -= VM_PropertyChanged;
+        }
+
+        private void LoadLanguageMenu()
+        {
+            //should never happen
+            if (LanguagesMenu.Items == null) return;
+
+            LanguagesMenu.Items.Clear();
+            foreach (var language in VM.Languages)
+            {
+                //add each language as menu item and hook up check event
+                ToggleMenuFlyoutItem menuItem = new ToggleMenuFlyoutItem();
+                //store the related language as tag
+                menuItem.Tag = language;
+                menuItem.Text = Localizer.GetString(language.Name);
+                if (language == VM.SelectedLanguage)
+                {
+                    //check
+                    menuItem.IsChecked = true;
+                }
+                menuItem.Tapped += LanguageMenuItemTapped;
+                LanguagesMenu.Items?.Add(menuItem);
+            }
+            VM.PropertyChanged += VM_PropertyChanged;
+        }
+
+        private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(VM.SelectedLanguage))
+            {
+                //select the right menu item
+                foreach (var menuItem in LanguagesMenu.Items.OfType<ToggleMenuFlyoutItem>())
+                {
+                    menuItem.IsChecked = menuItem.Tag == VM.SelectedLanguage;
+                }
+            }
+        }
+
+        private void LanguageMenuItemTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var menuItem = sender as ToggleMenuFlyoutItem;
+            if (menuItem != null)
+            {
+                //update Selected language
+                VM.SelectedLanguage = menuItem.Tag as GameLanguage;
+            }
         }
 
         private void ShowHideTips_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
@@ -27,7 +87,7 @@ namespace OmegaGo.UI.WindowsUniversal.Views
                 : Windows.UI.Xaml.Visibility.Collapsed;
         }
 
-        private void GoFullScreen_Click( object sender, Windows.UI.Xaml.RoutedEventArgs e )
+        private void GoFullScreen_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             FullscreenModeManager.Toggle();
         }
