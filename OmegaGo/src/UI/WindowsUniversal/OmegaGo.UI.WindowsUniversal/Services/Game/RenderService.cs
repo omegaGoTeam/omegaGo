9776e3b357150a1319e4c126467bf1b53ce31ff6
Petr Hudeček <petrhudecek2010@gmail.com>
2017-01-21 13:23:27 +0100

    showcoordinates is applied

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Services/Game/RenderService.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Services/Game/RenderService.cs
@@ -63,7 +63,10 @@ namespace OmegaGo.UI.WindowsUniversal.Services.Game
 
             // TODO Perf. optimalization: Place drawing board and coordinates into a command list.
 
-            DrawBoardCoordinates(sender, args.DrawingSession, boardWidth, boardHeight);
+            if (_settings.Display.ShowCoordinates)
+            {
+                DrawBoardCoordinates(sender, args.DrawingSession, boardWidth, boardHeight);
+            }
             args.DrawingSession.Transform = Matrix3x2.CreateTranslation(SharedBoardState.BoardBorderThickness, SharedBoardState.BoardBorderThickness);
             DrawBoardLines(args.DrawingSession, boardWidth, boardHeight);
             DrawBoardStarPoints(args.DrawingSession, SharedBoardState);
