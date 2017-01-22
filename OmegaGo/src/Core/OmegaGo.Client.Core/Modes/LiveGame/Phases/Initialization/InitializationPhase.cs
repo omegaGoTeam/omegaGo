8e6abbc9d4751750babc3fbef83d62f0366e76ad
Martin Zikmund <martinzikmund@live.com>
2017-01-22 10:40:34 +0100

    Initialization phase implemented
    Related Work Items: #386

--- OmegaGo/src/Core/OmegaGo.Client.Core/Modes/LiveGame/Phases/Initialization/InitializationPhase.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Modes/LiveGame/Phases/Initialization/InitializationPhase.cs
@@ -1,33 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OmegaGo.Core.Modes.LiveGame.Phases
+﻿namespace OmegaGo.Core.Modes.LiveGame.Phases.Initialization
 {
-    class InitializationPhase : IInitializationPhase
+    class InitializationPhase : GamePhaseBase, IInitializationPhase
     {
-        public GamePhaseType PhaseType => GamePhaseType.Initialization;
+        public InitializationPhase(IGameController gameController) : base(gameController)
+        {
+        }
+
+        public override GamePhaseType PhaseType => GamePhaseType.Initialization;
 
         /// <summary>
         /// Begins the main game loop by asking the first player (who plays black) to make a move, and then the second player, then the first,
         /// and so on until the game concludes. This method will return immediately but it will launch this loop in a Task on another thread.
         /// </summary>
-        public void BeginGame()
+        private void BeginGame()
         {
-            foreach (var player in _game.Players)
-            {
-                player.Agent.GameBegins(player, _game);
-            }
             _game.NumberOfMovesPlayed = 0;
-            SetGamePhase(GamePhase.MainPhase);
-            MainPhase_AskPlayerToMove(_game.Black);
+            foreach (var player in Controller.Players)
+            {
+                player.Agent.GameInitialized();
+            }
         }
 
         public void StartPhase()
         {
-            
+            BeginGame();
+            SetGamePhase(GamePhaseType.HandicapPlacement);
+            MainPhase_AskPlayerToMove(_game.Black);
         }
     }
 }
