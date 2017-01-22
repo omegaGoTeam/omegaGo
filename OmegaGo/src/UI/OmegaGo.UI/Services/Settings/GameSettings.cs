709389ddde61073aa4c9f325eabd8880a8d49a22
Martin Zikmund <martinzikmund@live.com>
2017-01-22 12:17:16 +0100

    Updated GameSettings.Tsumego_ShowPossibleMoves not to use nameof

--- OmegaGo/src/UI/OmegaGo.UI/Services/Settings/GameSettings.cs
+++ OmegaGo/src/UI/OmegaGo.UI/Services/Settings/GameSettings.cs
@@ -14,25 +14,28 @@ namespace OmegaGo.UI.Services.Settings
     {
         private readonly ISettingsService _settings;
 
-        public GameSettings( ISettingsService settings )
+        public GameSettings(ISettingsService settings)
         {
             _settings = settings;
         }
 
-        private const string LanguageSettingKey = "Language";
+        private const string TsumegoPossibleMovesSettingKey = "Tsumego_ShowPossibleMoves";
 
         public bool Tsumego_ShowPossibleMoves
         {
-            get { return _settings.GetSetting(nameof(Tsumego_ShowPossibleMoves), () => true); }
-            set { _settings.SetSetting(nameof(Tsumego_ShowPossibleMoves), value); }
+            get { return _settings.GetSetting(TsumegoPossibleMovesSettingKey, () => true); }
+            set { _settings.SetSetting(TsumegoPossibleMovesSettingKey, value); }
         }
+
+        private const string LanguageSettingKey = "Language";
+
         public string Language
         {
-            get { return _settings.GetSetting( LanguageSettingKey, () => GameLanguages.DefaultLanguage.CultureTag, SettingLocality.Roamed ); }
+            get { return _settings.GetSetting(LanguageSettingKey, () => GameLanguages.DefaultLanguage.CultureTag, SettingLocality.Roamed); }
             set
             {
-                if ( !GameLanguages.SupportedLanguages.ContainsKey( value ) ) throw new ArgumentException( nameof( value ) );
-                _settings.SetSetting( LanguageSettingKey, value, SettingLocality.Roamed );
+                if (!GameLanguages.SupportedLanguages.ContainsKey(value)) throw new ArgumentException(nameof(value));
+                _settings.SetSetting(LanguageSettingKey, value, SettingLocality.Roamed);
             }
         }
     }
