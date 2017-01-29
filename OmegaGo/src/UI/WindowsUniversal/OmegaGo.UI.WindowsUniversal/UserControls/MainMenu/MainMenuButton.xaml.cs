bac7e33be1dc06149d0c2df16d52f88bfca17443
Martin Zikmund <martinzikmund@live.com>
2017-01-30 00:24:48 +0100

    First draft of adaptive buttons

---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/MainMenu/MainMenuButton.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OmegaGo.UI.WindowsUniversal.UserControls.MainMenu
+{
+    public sealed partial class MainMenuButton : UserControl
+    {
+        public MainMenuButton()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command", typeof(ICommand), typeof(MainMenuButton), new PropertyMetadata(default(ICommand)));
+
+        public ICommand Command
+        {
+            get { return (ICommand) GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(MainMenuButton), new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get { return (string) GetValue(TextProperty); }
+            set { SetValue(TextProperty, value ); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            "Icon", typeof(UIElement), typeof(MainMenuButton), new PropertyMetadata(default(UIElement)));
+
+        public UIElement Icon
+        {
+            get { return (UIElement) GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolTipProperty = DependencyProperty.Register(
+            "ToolTip", typeof(string), typeof(MainMenuButton), new PropertyMetadata(default(string)));
+
+        public string ToolTip
+        {
+            get { return (string) GetValue(ToolTipProperty); }
+            set { SetValue(ToolTipProperty, value); }
+        }
+    }
+}
