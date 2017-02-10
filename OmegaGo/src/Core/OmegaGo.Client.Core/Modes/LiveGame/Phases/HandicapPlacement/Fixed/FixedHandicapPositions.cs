6b810d4dd471c47311dae7763ba03b798d58233e
Martin Zikmund <martinzikmund@live.com>
2017-02-10 13:41:29 +0100

    Documentation of fixed handicap positions class

--- OmegaGo/src/Core/OmegaGo.Client.Core/Modes/LiveGame/Phases/HandicapPlacement/Fixed/FixedHandicapPositions.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Modes/LiveGame/Phases/HandicapPlacement/Fixed/FixedHandicapPositions.cs
@@ -5,37 +5,54 @@ using OmegaGo.Core.Game;
 
 namespace OmegaGo.Core.Modes.LiveGame.Phases.HandicapPlacement.Fixed
 {
+    /// <summary>
+    /// Provides fixed handicap positions
+    /// </summary>
     public static class FixedHandicapPositions
     {
-        public const int MaxFixedHandicap9 = 5;
-        public const int MaxFixedHandicap13 = 9;
-        public const int MaxFixedHandicap19 = 9;
+        /// <summary>
+        /// Fixed handicap positions for 9x9 board
+        /// </summary>
+        private static readonly Position[] FixedHandicapPositions9 =
+        {
+            new Position(6, 6),
+            new Position(2, 2),
+            new Position(6, 2),
+            new Position(2, 6),
+            new Position(4, 4)
+        };
 
-        public static readonly Position[] FixedHandicapPositions9 = new Position[] { new Position(6, 6),
-                                                                     new Position(2, 2),
-                                                                     new Position(6, 2),
-                                                                     new Position(2, 6),
-                                                                     new Position(4, 4) };
+        /// <summary>
+        /// Fixed handicap positions 13x13 board
+        /// </summary>
+        private static readonly Position[] FixedHandicapPositions13 =
+        {
+            new Position(9, 9),
+            new Position(3, 3),
+            new Position(9, 3),
+            new Position(3, 9),
+            new Position(9, 6),
+            new Position(3, 6),
+            new Position(6, 9),
+            new Position(6, 3),
+            new Position(6, 6)
+        };
 
-        public static readonly Position[] FixedHandicapPositions13 = new Position[] { new Position(9, 9),
-                                                                      new Position(3, 3),
-                                                                      new Position(9, 3),
-                                                                      new Position(3, 9),
-                                                                      new Position(9, 6),
-                                                                      new Position(3, 6),
-                                                                      new Position(6, 9),
-                                                                      new Position(6, 3),
-                                                                      new Position(6, 6) };
-
-        public static readonly Position[] FixedHandicapPositions19 = new Position[] { new Position(15, 15),
-                                                                      new Position(3, 3),
-                                                                      new Position(15, 3),
-                                                                      new Position(3, 15),
-                                                                      new Position(15, 9),
-                                                                      new Position(3, 9),
-                                                                      new Position(9, 15),
-                                                                      new Position(9, 3),
-                                                                      new Position(9, 9) };
+        /// <summary>
+        /// Fixed handic positions for 19x19 boards
+        /// </summary>
+        private static readonly Position[] FixedHandicapPositions19 =
+        {
+            new Position(15, 15),
+            new Position(3, 3),
+            new Position(15, 3),
+            new Position(3, 15),
+            new Position(15, 9),
+            new Position(3, 9),
+            new Position(9, 15),
+            new Position(9, 3),
+            new Position(9, 9)
+        };
 
         /// <summary>
         /// Returns the positions of handicap stones
@@ -45,31 +62,31 @@ namespace OmegaGo.Core.Modes.LiveGame.Phases.HandicapPlacement.Fixed
         /// <returns>Positions of handicap stones</returns>
         public static IEnumerable<Position> GetHandicapStonePositions(GameBoardSize size, int handicap)
         {
-            if ( !size.IsSquare) throw new ArgumentOutOfRangeException(nameof(size), "Invalid game board size for fixed handicap");
+            if (!size.IsSquare) throw new ArgumentOutOfRangeException(nameof(size), "Invalid game board size for fixed handicap");
             Position[] sourceArray = null;
             switch (size.Width)
             {
                 case 9:
-                {
-                    sourceArray = FixedHandicapPositions9;
-                    break;
-                }
+                    {
+                        sourceArray = FixedHandicapPositions9;
+                        break;
+                    }
                 case 13:
-                {
-                    sourceArray = FixedHandicapPositions13;
-                    break;
-                }
+                    {
+                        sourceArray = FixedHandicapPositions13;
+                        break;
+                    }
                 case 19:
-                {
-                    sourceArray = FixedHandicapPositions19;
-                    break;
-                }
+                    {
+                        sourceArray = FixedHandicapPositions19;
+                        break;
+                    }
                 default:
                     throw new ArgumentOutOfRangeException(nameof(size), "Invalid game board size for fixed handicap");
             }
 
-            if ( handicap > sourceArray.Length)
-                throw new ArgumentOutOfRangeException(nameof(handicap),"Too many handicap stones requested");
+            if (handicap > sourceArray.Length)
+                throw new ArgumentOutOfRangeException(nameof(handicap), "Too many handicap stones requested");
 
             return sourceArray.Take(handicap);
         }
