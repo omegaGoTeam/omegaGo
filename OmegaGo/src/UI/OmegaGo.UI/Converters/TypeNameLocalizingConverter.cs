9e0dc6fe3be90f9d28d2ec8dd1be8a412d097e23
Martin Zikmund <martinzikmund@live.com>
2017-02-11 16:34:48 +0100

    TypeNameLocalizingConverter in UI PCL
    Related Work Items: #470

---
+++ OmegaGo/src/UI/OmegaGo.UI/Converters/TypeNameLocalizingConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Platform.Converters;
+
+namespace OmegaGo.UI.Converters
+{
+    /// <summary>
+    /// Localizing converter using the TypeName of the provided instance.
+    /// ConverterParameter can also provide a format string.
+    /// </summary>
+    public class TypeNameLocalizingConverter : MvxValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var typeName = value.GetType().Name;
+            var formatString = (parameter as string) ?? "{0}";
+            return string.Format(formatString, typeName);
+        }
+    }
+}
