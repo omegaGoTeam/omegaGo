5b9c6c296889ad8a5908c1b9ebd25e249b072114
Martin Zikmund <martinzikmund@live.com>
2017-02-21 17:39:33 +0100

    Changed HelpPage file structure and added comments

--- OmegaGo/src/UI/OmegaGo.UI/Services/Help/HelpPage.cs
+++ OmegaGo/src/UI/OmegaGo.UI/Services/Help/HelpPage.cs
@@ -11,30 +11,47 @@ namespace OmegaGo.UI.Services.Help
     /// </summary>
     public class HelpPage
     {
-        private string _name;
-        private string _filename;
+        /// <summary>
+        /// Filename of the help page
+        /// </summary>
+        private readonly string _fileName;
 
-        public string Name
+        /// <summary>
+        /// Stores previously loaded content for faster retrieval
+        /// </summary>
+        private string _cachedContent = null;
+
+        /// <summary>
+        /// Creates a help page
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="fileName">File name</param>
+        private HelpPage(string name, string fileName)
         {
-            get { return _name; }
+            Name = name;
+            _fileName = fileName;
         }
 
-        private HelpPage(string name, string filename)
-        {
-            this._name = name;
-            this._filename = filename;
-        }
+        /// <summary>
+        /// Name of the help page
+        /// </summary>
+        public string Name { get; }
 
         /// <summary>
         /// Gets the HTML content of this page by reading it from a corresponding embedded resource and sandwiching it between the header and the footer HTML. This is regenerated each time the page is shown to the user.
         /// </summary>
         public string Content
         {
-            get {
-                string header = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data.header.html");
-                string footer = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data.footer.html");
-                string content = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data." + this._filename + ".html");
-                return header + content + footer;
+            get
+            {
+                if (_cachedContent == null)
+                {
+                    string header = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data.header.html");
+                    string footer = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data.footer.html");
+                    string content = EmbeddedResourceReading.ReadAllText("OmegaGo.UI.Services.Help.Data." + this._fileName + ".html");
+                    _cachedContent = header + content + footer;
+                }
+                return _cachedContent;
             }
         }
 
@@ -55,7 +72,7 @@ namespace OmegaGo.UI.Services.Help
                 new HelpPage("Jargon", "jargon"),
                 new HelpPage("Resources", "resources"),
                 new HelpPage("Humour", "humour"),
-                new HelpPage("Copyright notice", "copyright"),                
+                new HelpPage("Copyright notice", "copyright"),
                 new HelpPage("Credits", "credits")
             };
         }
