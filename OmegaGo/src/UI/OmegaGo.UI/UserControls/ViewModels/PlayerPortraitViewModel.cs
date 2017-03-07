a1b08ae3b76d4cda6af4c926542f125bf69f4dd2
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2017-03-07 17:02:21 +0100

    Sync commit

--- OmegaGo/src/UI/OmegaGo.UI/UserControls/ViewModels/PlayerPortraitViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/UserControls/ViewModels/PlayerPortraitViewModel.cs
@@ -1,41 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OmegaGo.Core.AI.Joker23.Players;
-using OmegaGo.Core.Game;
+﻿using OmegaGo.Core.Game;
 using OmegaGo.Core.Modes.LiveGame.Players;
 using OmegaGo.Core.Time;
-using OmegaGo.UI.ViewModels;
 
 namespace OmegaGo.UI.UserControls.ViewModels
 {
-    public class PlayerPortraitViewModel : ControlViewModelBase
+    public sealed class PlayerPortraitViewModel : ControlViewModelBase
     {
-        public string Name => _player.Info.Name + " (" + _player.Info.Rank + ")";
+        private GamePlayer _player;
+        private string _timeControlMainLine = "f";
+        private string _timeControlSubLine = "f";
+
+        public string Name => $"{_player.Info.Name} ({_player.Info.Rank})";
         public StoneColor Color => _player?.Info?.Color ?? StoneColor.Black;
         public TimeControl Clock => _player.Clock;
-
-        private string _timeControlMainLine = "f";
-
+        
         public string TimeControlMainLine
         {
             get { return _timeControlMainLine; }
             set { SetProperty(ref _timeControlMainLine, value); }
         }
-        private string _timeControlSubLine = "f";
-
+        
         public string TimeControlSubLine
         {
             get { return _timeControlSubLine; }
             set { SetProperty(ref _timeControlSubLine, value); }
         }
-        private GamePlayer _player;
-
+        
         public PlayerPortraitViewModel(GamePlayer player)
         {
-            this._player = player;
+            _player = player;
         }
 
         public void Update()
