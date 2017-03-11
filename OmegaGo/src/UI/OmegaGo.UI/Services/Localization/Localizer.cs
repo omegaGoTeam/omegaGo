6680337a3f6007e0ddb6978f6e46e36bd53f0762
Martin Zikmund <martinzikmund@live.com>
2017-03-11 12:54:15 +0100

    Statistics view design with item grid

--- OmegaGo/src/UI/OmegaGo.UI/Services/Localization/Localizer.cs
+++ OmegaGo/src/UI/OmegaGo.UI/Services/Localization/Localizer.cs
@@ -193,7 +193,7 @@ namespace OmegaGo.UI.Services.Localization
         public string QuestsCompleted => LocalizeCaller();
         public string TsumegoProblemsSolved => LocalizeCaller();
         public string PointsColon => LocalizeCaller();
-        public string OmegaGoRankColon => LocalizeCaller();
+        public string OmegaGoRank => LocalizeCaller();
 
         // Board settings control strings
         public string BoardSettings => LocalizeCaller();
@@ -208,6 +208,8 @@ namespace OmegaGo.UI.Services.Localization
         public string Rank5 => LocalizeCaller();
         public string YouHaveAdvancedToNewRankX => LocalizeCaller();
         public string YouHaveGainedXPointsNowYouHaveY => LocalizeCaller();
+
+        public string ResetAllProgress => LocalizeCaller();
         public string ResetAllProgress_Content => LocalizeCaller();
         public string ResetAllProgress_Caption => LocalizeCaller();
         public string ResetAllProgress_Yes => LocalizeCaller();
@@ -239,8 +241,8 @@ namespace OmegaGo.UI.Services.Localization
         public string Tsumego_WrongPanel => LocalizeCaller();
         public string Tsumego_CurrentProblemStatus => LocalizeCaller();
         public string Tsumego_ViewCaption => LocalizeCaller();        
-        public string IgsRankColon => LocalizeCaller();
-        public string KgsRankColon => LocalizeCaller();
+        public string IgsRank => LocalizeCaller();
+        public string KgsRank => LocalizeCaller();
         public string Tutorial_Next => LocalizeCaller();
         public string MuteTooltip => LocalizeCaller();
         public string SingleplayerMenu => LocalizeCaller();
