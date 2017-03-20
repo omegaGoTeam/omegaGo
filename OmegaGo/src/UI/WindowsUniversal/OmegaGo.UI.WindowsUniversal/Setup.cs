0d5a17913ab888eeed6c24291424f7d3fc40a16d
Martin Zikmund <martinzikmund@live.com>
2017-03-20 23:13:53 +0100

    Creating the omegaGo view presenter instead of the default

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Setup.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Setup.cs
@@ -5,6 +5,7 @@ using MvvmCross.WindowsUWP.Platform;
 using OmegaGo.UI.Services;
 using OmegaGo.UI.WindowsUniversal.Services;
 using Windows.UI.Xaml.Controls;
+using MvvmCross.WindowsUWP.Views;
 using OmegaGo.UI.Services.Audio;
 using OmegaGo.UI.Services.Dialogs;
 using OmegaGo.UI.Services.Settings;
@@ -13,6 +14,7 @@ using OmegaGo.UI.WindowsUniversal.Services.Settings;
 using OmegaGo.UI.Services.Files;
 using OmegaGo.UI.Services.Notifications;
 using OmegaGo.UI.Services.PasswordVault;
+using OmegaGo.UI.WindowsUniversal.Infrastructure;
 using OmegaGo.UI.WindowsUniversal.Services.Audio;
 using OmegaGo.UI.WindowsUniversal.Services.Dialogs;
 using OmegaGo.UI.WindowsUniversal.Services.Notifications;
@@ -48,6 +50,16 @@ namespace OmegaGo.UI.WindowsUniversal
             Mvx.LazyConstructAndRegisterSingleton<IAppNotificationService, AppNotificationService>();
 
             base.InitializeFirstChance();
+        }
+
+        /// <summary>
+        /// Creates the omegaGo view presenter
+        /// </summary>
+        /// <param name="rootFrame">Root frame</param>
+        /// <returns>View presenter</returns>
+        protected override IMvxWindowsViewPresenter CreateViewPresenter(IMvxWindowsFrame rootFrame)
+        {
+            return new OmegaGoViewPresenter(rootFrame);
         }
     }
 }
