7f70523f81619d77b74783663884871f1f4da89e
Anikó Kuklis <kuklisaniko@gmail.com>
2017-03-26 13:30:59 +0200

    Added reference to Rules

--- OmegaGo/src/Core/OmegaGo.Client.Core/Rules/RulesetType.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Rules/RulesetType.cs
@@ -14,14 +14,17 @@ namespace OmegaGo.Core.Rules
     {
         /// <summary>
         /// These are used by the American Go Association.
+        /// Rules: http://www.usgo.org/files/pdf/completerules.pdf
         /// </summary>
         AGA,
         /// <summary>
         /// This is the other major set of rules in widespread use, also known as "area" rules.
+        /// Rules: https://docs.google.com/document/d/1bslniW1jrpUAs2o5xJJiXKol8v-E0-mxqowa3LZ8Kt0/edit
         /// </summary>
         Chinese,
         /// <summary>
         /// These are rules used in Japan, sometimes known as "territory" rules. 
+        /// Rules: http://www.cs.cmu.edu/~wjh/go/rules/Japanese.html
         /// </summary>
         Japanese
     }
