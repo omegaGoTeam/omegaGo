b6a5a1d48762f4ade409a7818a70a196a6eb5e86
Petr Hudeček <petrhudecek2010@gmail.com>
2017-04-01 11:23:59 +0200

    reorder

--- OmegaGo/src/UI/OmegaGo.UI/ViewModels/KgsHomeViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/ViewModels/KgsHomeViewModel.cs
@@ -1,20 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
-using MvvmCross.Platform;
-using OmegaGo.Core.Modes.LiveGame;
-using OmegaGo.Core.Modes.LiveGame.Remote.Igs;
-using OmegaGo.Core.Online;
-using OmegaGo.Core.Online.Igs;
-using OmegaGo.Core.Online.Igs.Structures;
-using OmegaGo.UI.Extensions;
-using OmegaGo.UI.Services.GameCreation;
-using OmegaGo.UI.Services.GameCreationBundle;
 using OmegaGo.UI.Services.Online;
 using OmegaGo.UI.Services.Settings;
 using OmegaGo.UI.UserControls.ViewModels;
@@ -24,18 +9,13 @@ namespace OmegaGo.UI.ViewModels
     public class KgsHomeViewModel : ViewModelBase
     {
         private readonly IGameSettings _settings;
-       
+
 
         public KgsHomeViewModel(IGameSettings settings)
         {
             this._settings = settings;
             this.LoginForm = new KgsLoginForm(this.Localizer, this._settings);
             this.LoginForm.LoginClick += LoginForm_LoginClick;
-        }
-
-        private async void LoginForm_LoginClick(object sender, LoginEventArgs e)
-        {
-            await AttemptLoginCommand(e.Username, e.Password);
         }
 
 
@@ -45,25 +25,32 @@ namespace OmegaGo.UI.ViewModels
             this.LoginForm.FormVisible = true;
             this.LoginForm.FormEnabled = true;
         });
+
         public LoginFormViewModel LoginForm { get; }
+
         public async Task AttemptLoginCommand(string username, string password)
         {
-            LoginForm.LoginErrorMessageOpacity = 1;
-            LoginForm.FormEnabled = false;
-            
-            LoginForm.LoginErrorMessage = "Logging in as " + username + "...";
+            this.LoginForm.LoginErrorMessageOpacity = 1;
+            this.LoginForm.FormEnabled = false;
+
+            this.LoginForm.LoginErrorMessage = "Logging in as " + username + "...";
             bool loginSuccess = await Connections.Kgs.LoginAsync(username, password);
-            LoginForm.FormEnabled = true;
+            this.LoginForm.FormEnabled = true;
             if (loginSuccess)
             {
-                LoginForm.FormVisible = false;
-                LoginForm.LoginErrorMessageOpacity = 0;
+                this.LoginForm.FormVisible = false;
+                this.LoginForm.LoginErrorMessageOpacity = 0;
             }
             else
             {
-                LoginForm.LoginErrorMessage = "The username or password you entered is incorrect.";
-                LoginForm.LoginErrorMessageOpacity = 1;
+                this.LoginForm.LoginErrorMessage = "The username or password you entered is incorrect.";
+                this.LoginForm.LoginErrorMessageOpacity = 1;
             }
+        }
+
+        private async void LoginForm_LoginClick(object sender, LoginEventArgs e)
+        {
+            await AttemptLoginCommand(e.Username, e.Password);
         }
     }
 }
