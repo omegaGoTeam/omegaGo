2e14a960c8d09cf9d0fa0cce6564f96ff949e2ab
Anikó Kuklis <kuklisaniko@gmail.com>
2017-04-14 16:10:59 +0200

    new ToolKind

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Game/Tools/ToolKind.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.Core.Game.Tools
+{
+    public enum ToolKind
+    {
+        Circle,
+        Cross,
+        Letter,
+        Number,
+        Square,
+        Triangle,
+        StonePlacement,
+        DeleteBranch
+    }
+}
