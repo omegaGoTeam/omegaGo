00969f9db00e61c80533d66bf86eb7119008a033
Martin Zikmund <martinzikmund@live.com>
2017-04-28 14:55:24 +0200

    Added app package service
    Related Work Items: #675

---
+++ OmegaGo/src/UI/OmegaGo.UI/Services/AppPackage/IAppPackageService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaGo.UI.Services.AppPackage
+{
+    /// <summary>
+    /// Provides access to information about the application package
+    /// </summary>
+    public interface IAppPackageService
+    {
+        /// <summary>
+        /// App name
+        /// </summary>
+        string AppName { get; }
+
+        /// <summary>
+        /// App version
+        /// </summary>
+        string Version { get; }
+    }
+}
