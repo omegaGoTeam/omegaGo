d8e84545e5b40096e386160d1fea9d7ac17dc77a
Petr Hudeček <petrhudecek2010@gmail.com>
2017-05-04 17:15:25 +0200

    removing the possibility from KGS and adjusting visibility

--- OmegaGo/src/UI/OmegaGo.UI/ViewModels/OnlineGameViewModel.cs
+++ OmegaGo/src/UI/OmegaGo.UI/ViewModels/OnlineGameViewModel.cs
@@ -39,7 +39,7 @@ namespace OmegaGo.UI.ViewModels
             set { SetProperty(ref _canAgreeOrDisagreeUndo, value); }
         }
 
-        public override bool ResumingGameIsPossible => !(Game.Info is IgsGameInfo);
+        public bool IsIgs => (Game.Info is IgsGameInfo);
 
         /// <summary>
         /// Agree with undo command
