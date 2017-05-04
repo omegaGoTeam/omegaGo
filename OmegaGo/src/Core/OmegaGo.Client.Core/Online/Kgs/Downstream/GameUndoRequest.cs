1bb13a6e8ab2bc5b62a3f7b24561cf17b52b34df
Petr Hudeček <petrhudecek2010@gmail.com>
2017-05-04 21:15:14 +0200

    accept and refuse KGS undo

---
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Online/Kgs/Downstream/GameUndoRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmegaGo.Core.Online.Kgs.Downstream.Abstract;
+
+namespace OmegaGo.Core.Online.Kgs.Downstream
+{
+    /// <summary>
+    /// User requested an undo. Ignore it to reject it, or send GAME_UNDO_ACCEPT to let the undo happen.
+    /// </summary>
+    /// <seealso cref="OmegaGo.Core.Online.Kgs.Downstream.Abstract.KgsInterruptChannelMessage" />
+    class GameUndoRequest : KgsInterruptChannelMessage
+    {
+        public override void Process(KgsConnection connection)
+        {
+            var myGame = connection.Data.GetGame(this.ChannelId);
+            connection.Events.RaiseUndoRequestReceived(myGame);
+        }
+    }
+}
