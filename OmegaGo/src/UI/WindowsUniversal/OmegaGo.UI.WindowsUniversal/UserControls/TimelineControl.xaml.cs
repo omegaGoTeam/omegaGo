2ba1f05b9b4164cc32b93bae761d13f32db9ac50
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2017-05-06 20:04:45 +0200

    Added keyboard arrows navigation for the timeline control.

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/TimelineControl.xaml.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/TimelineControl.xaml.cs
@@ -10,6 +10,9 @@ using OmegaGo.Core.Game;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Text;
+using Windows.System;
+using System.Threading.Tasks;
+using Windows.UI.Core;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -21,6 +24,12 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
         private const int NODESPACING = 4;
         private const int NODEHIGHLIGHTSTROKE = 2;
         private const int NODEFONTSIZE = 10;
+
+        private static readonly Color WhiteNodeColor = Colors.White;
+        private static readonly Color BlackNodeColor = Colors.Black;
+        private static readonly Color EmptyNodeColor = Colors.Maroon;
+        private static readonly Color HighlightNodeColor = Colors.Tomato;
+        private static readonly Color LineColor = Colors.Gray;
 
         public static readonly DependencyProperty ViewModelProperty =
                 DependencyProperty.Register(
@@ -42,7 +51,9 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
             {
                 TimelineViewModel viewModel = (TimelineViewModel)e.NewValue;
                 viewModel.TimelineRedrawRequested += timelineControl.TimelineRedrawRequsted;
-
+                
+                timelineControl.PointerReleased += timelineControl.TimelineControl_PointerReleased;
+                timelineControl.KeyUp += timelineControl.TimelineControl_KeyUp;
                 timelineControl.canvas.Draw += timelineControl.Canvas_Draw;
                 timelineControl.canvas.PointerReleased += timelineControl.Canvas_PointerReleased;
                 timelineControl.canvas.Invalidate();
@@ -70,10 +81,10 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
 
         protected override Size MeasureOverride(Size availableSize)
         {
-            if (ViewModel?.GameTree == null || ViewModel?.GameTree?.GameTreeRoot.Branches.Count == 0)
+            if (ViewModel?.GameTree == null || ViewModel?.GameTree?.GameTreeRoot == null)
                 return base.MeasureOverride(availableSize);
 
-            int requiredHeight = CalculateDesiredSize(ViewModel.GameTree.GameTreeRoot.Branches[0], 0, 0) + 1;
+            int requiredHeight = CalculateDesiredSize(ViewModel.GameTree.GameTreeRoot, 0, 0) + 1;
             
             Size desiredSize = new Size();
             desiredSize.Height =
@@ -105,10 +116,10 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
 
         private void Canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-            if (ViewModel.GameTree != null && ViewModel.GameTree.GameTreeRoot.Branches.Count != 0)
+            if (ViewModel.GameTree != null && ViewModel.GameTree.GameTreeRoot != null)
             {
                 args.DrawingSession.Transform = Matrix3x2.CreateTranslation(NODEHIGHLIGHTSTROKE, NODEHIGHLIGHTSTROKE);
-                int requiredHeight = DrawNode(args.DrawingSession, ViewModel.GameTree.GameTreeRoot.Branches[0], 0, 0, 0) + 1;
+                int requiredHeight = DrawNode(args.DrawingSession, ViewModel.GameTree.GameTreeRoot, 0, 0, 0) + 1;
             }
         }
 
@@ -121,7 +132,12 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
             pointerPosition.X -= NODEHIGHLIGHTSTROKE;
             pointerPosition.Y -= NODEHIGHLIGHTSTROKE;
 
-            GetNodeAtPoint(pointerPosition, ViewModel.GameTree.GameTreeRoot.Branches[0], 0, 0, out pressedNode);
+            // First node is empty, ignor it
+            if (ViewModel.GameTree.GameTreeRoot == null)
+                return;
+
+            GameTreeNode gameTreeRootNode = ViewModel.GameTree.GameTreeRoot;
+            GetNodeAtPoint(pointerPosition, gameTreeRootNode, 0, 0, out pressedNode);
 
             if (pressedNode != null && pressedNode != ViewModel.SelectedTimelineNode)
             {
@@ -133,6 +149,7 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
         {
             int nodeOffset = offset;
 
+            // Calculate node position
             Vector2 stoneTopLeft = new Vector2(
                 depth * NODESIZE + depth * NODESPACING,
                 offset * NODESIZE + offset * NODESPACING);
@@ -141,33 +158,41 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
             stoneCenter.X += NODESIZE * 0.5f;
             stoneCenter.Y += NODESIZE * 0.5f;
 
-            string nodeText="";
+            string nodeText = "";
+
+            // Get text for node
             if (node.Move.Kind == MoveKind.PlaceStone)
                 nodeText = node.Move.Coordinates.ToIgsCoordinates();
             else if (node.Move.Kind == MoveKind.Pass)
                 nodeText = "P";
 
             CanvasTextLayout textLayout = GetTextLayoutForString(drawingSession.Device, nodeText);
+            // Draw node according to the color of player whose move that was
             if (node.Move.WhoMoves == StoneColor.Black)
             {
-                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, Colors.Black);
+                // Black move
+                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, BlackNodeColor);
                 drawingSession.DrawTextLayout(textLayout, stoneTopLeft, Colors.White);
             }
             else if (node.Move.WhoMoves == StoneColor.White)
             {
-                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, Colors.White);
+                // White move
+                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, WhiteNodeColor);
                 drawingSession.DrawTextLayout(textLayout, stoneTopLeft, Colors.Black);
             }
             else
             {
-                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, Colors.MediumPurple);
+                // Empty node, no move
+                drawingSession.FillEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, EmptyNodeColor);
             }
 
+            // If the current node is the selected node, draw highlight
             if (node == ViewModel.SelectedTimelineNode)
             {
-                drawingSession.DrawEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, Colors.Tomato, NODEHIGHLIGHTSTROKE);
+                drawingSession.DrawEllipse(stoneCenter, NODESIZE * 0.5f, NODESIZE * 0.5f, HighlightNodeColor, NODEHIGHLIGHTSTROKE);
             }
 
+            // Calculate starting and end points for line connecting two nodes
             Vector2 lineStart = new Vector2(
                 (depth) * NODESIZE + (depth - 1) * NODESPACING, 
                 (parentOffset) * NODESIZE + parentOffset * NODESPACING + NODESIZE * 0.5f);
@@ -175,8 +200,10 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
                 depth * NODESIZE + depth * NODESPACING,
                 offset * NODESIZE + offset * NODESPACING + NODESIZE * 0.5f);
 
-            drawingSession.DrawLine(lineStart, lineEnd, Colors.Gray);
+            // Draw line between new node and its parent
+            drawingSession.DrawLine(lineStart, lineEnd, LineColor);
             
+            // Draw children nodes
             foreach(GameTreeNode childNode in node.Branches)
             {
                 offset = DrawNode(drawingSession, childNode, depth + 1, offset, nodeOffset);
@@ -251,6 +278,150 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls
             _textLayoutCache[text] = textLayout;
 
             return textLayout;
+        }
+
+        //////
+        // Keyboard arrows handling
+        //////
+
+        private void TimelineControl_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            // Hack around UWP XAML Focus WTFiness
+            Task.Run(
+                async () =>
+                {
+                    await Task.Delay(100);
+                    var task = this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => this.Focus(FocusState.Programmatic));
+                });
+        }
+
+        private void TimelineControl_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case VirtualKey.Left:
+                    SwitchToParentNode();
+                    break;
+                case VirtualKey.Right:
+                    SwitchToFirstChildNode();
+                    break;
+                case VirtualKey.Up:
+                    SwitchToPreviousMoveNumberNode();
+                    break;
+                case VirtualKey.Down:
+                    SwitchToNextMoveNumberNode();
+                    break;
+            }
+        }
+        
+        private void SwitchToPreviousMoveNumberNode()
+        {
+            GameTreeNode node = ViewModel.SelectedTimelineNode;
+            GameTreeNode nodeParent = ViewModel.SelectedTimelineNode.Parent;
+            int targetMoveNumber = node.MoveNumber;
+            int searchMoveNumber = targetMoveNumber;
+            
+            while (nodeParent != null)
+            {
+                int nodeBranchIndex = nodeParent.Branches.IndexOf(node);
+
+                // Search previous siblings
+                for (int i = nodeBranchIndex - 1; i >= 0; i--)
+                {
+                    GameTreeNode searchNode = SwitchToPreviousMoveNumberNodeDFS(nodeParent.Branches[i], searchMoveNumber, targetMoveNumber);
+
+                    if(searchNode != null)
+                    {
+                        ViewModel.SetSelectedNode(searchNode);
+                        return;
+                    }
+                }
+
+                node = nodeParent;
+                nodeParent = nodeParent.Parent;
+                searchMoveNumber--;
+            }
+        }
+
+        private GameTreeNode SwitchToPreviousMoveNumberNodeDFS(GameTreeNode node, int currentMoveIndex, int targetMoveIndex)
+        {
+            if (currentMoveIndex == targetMoveIndex)
+                return node;
+
+            int nodeChildrenCount = node.Branches.Count;
+
+            for (int i = nodeChildrenCount - 1; i >= 0; i--)
+            {
+                GameTreeNode searchNode = SwitchToPreviousMoveNumberNodeDFS(node.Branches[i], currentMoveIndex + 1, targetMoveIndex);
+
+                if (searchNode != null)
+                    return searchNode;
+            }
+
+            return null;
+        }
+
+        private void SwitchToNextMoveNumberNode()
+        {
+            GameTreeNode node = ViewModel.SelectedTimelineNode;
+            GameTreeNode nodeParent = ViewModel.SelectedTimelineNode.Parent;
+            int targetMoveNumber = node.MoveNumber;
+            int searchMoveNumber = targetMoveNumber;
+
+            while (nodeParent != null)
+            {
+                int nodeBranchIndex = nodeParent.Branches.IndexOf(node);
+
+                // Search previous siblings
+                for (int i = nodeBranchIndex + 1; i < nodeParent.Branches.Count; i++)
+                {
+                    GameTreeNode searchNode = SwitchToNextMoveNumberNodeDFS(nodeParent.Branches[i], searchMoveNumber, targetMoveNumber);
+
+                    if (searchNode != null)
+                    {
+                        ViewModel.SetSelectedNode(searchNode);
+                        return;
+                    }
+                }
+
+                node = nodeParent;
+                nodeParent = nodeParent.Parent;
+                searchMoveNumber--;
+            }
+        }
+
+        private GameTreeNode SwitchToNextMoveNumberNodeDFS(GameTreeNode node, int currentMoveIndex, int targetMoveIndex)
+        {
+            if (currentMoveIndex == targetMoveIndex)
+                return node;
+
+            int nodeChildrenCount = node.Branches.Count;
+
+            for (int i = 0; i < nodeChildrenCount; i++)
+            {
+                GameTreeNode searchNode = SwitchToNextMoveNumberNodeDFS(node.Branches[i], currentMoveIndex + 1, targetMoveIndex);
+
+                if (searchNode != null)
+                    return searchNode;
+            }
+
+            return null;
+        }
+
+        private void SwitchToFirstChildNode()
+        {
+            GameTreeNode node = ViewModel.SelectedTimelineNode;
+
+            if (node.NextNode != null)
+                ViewModel.SetSelectedNode(node.NextNode);
+        }
+
+        private void SwitchToParentNode()
+        {
+            GameTreeNode node = ViewModel.SelectedTimelineNode;
+
+            if (node.Parent != null)
+                ViewModel.SetSelectedNode(node.Parent);
         }
     }
 }
