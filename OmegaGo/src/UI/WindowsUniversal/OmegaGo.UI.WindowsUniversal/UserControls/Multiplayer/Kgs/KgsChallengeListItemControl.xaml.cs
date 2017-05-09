f51910b18b350f47259649627752aa8352082a6e
Martin Zikmund <martinzikmund@live.com>
2017-05-09 21:30:54 +0200

    Lobby binding fix

--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/Multiplayer/Kgs/KgsChallengeListItemControl.xaml.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/Multiplayer/Kgs/KgsChallengeListItemControl.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,13 +14,14 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using OmegaGo.Core.Annotations;
 using OmegaGo.Core.Online.Kgs.Structures;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer.Kgs
 {
-    public sealed partial class KgsChallengeListItemControl : UserControl
+    public sealed partial class KgsChallengeListItemControl : UserControl, INotifyPropertyChanged
     {
         public KgsChallengeListItemControl()
         {
@@ -26,7 +29,14 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer.Kgs
         }
 
         public static readonly DependencyProperty ChallengeProperty = DependencyProperty.Register(
-            "Challenge", typeof(KgsChallenge), typeof(KgsChallengeListItemControl), new PropertyMetadata(default(KgsChallenge)));
+            "Challenge", typeof(KgsChallenge), typeof(KgsChallengeListItemControl), new PropertyMetadata(default(KgsChallenge), ChallengeChanged));
+
+        private static void ChallengeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as KgsChallengeListItemControl;
+            control.OnPropertyChanged(nameof(Author));
+            control.OnPropertyChanged(nameof(AuthorRank));
+        }
 
         public KgsChallenge Challenge
         {
@@ -37,5 +47,13 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer.Kgs
         public string Author => Challenge.Proposal.Players.FirstOrDefault()?.User.Name;
 
         public string AuthorRank => Challenge.Proposal.Players.FirstOrDefault()?.User.Rank;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/Multiplayer/ObservedGameControl.xaml.cs
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/Multiplayer/ObservedGameControl.xaml.cs
@@ -4,12 +4,15 @@ using OmegaGo.Core.Game;
 using OmegaGo.Core.Online.Kgs.Structures;
 using OmegaGo.Core.Online.Igs;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using OmegaGo.Core.Annotations;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer
 {
-    public sealed partial class ObservedGameControl : UserControl
+    public sealed partial class ObservedGameControl : UserControl, INotifyPropertyChanged
     {
         public ObservedGameControl()
         {
@@ -17,8 +20,15 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer
         }
 
         public static readonly DependencyProperty GameInfoProperty = DependencyProperty.Register(
-            "GameInfo", typeof(GameInfo), typeof(ObservedGameControl), new PropertyMetadata(default(GameInfo)));
-        
+            "GameInfo", typeof(GameInfo), typeof(ObservedGameControl), new PropertyMetadata(default(GameInfo), GameInfoChanged));
+
+        private static void GameInfoChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ObservedGameControl;
+            control.OnPropertyChanged(nameof(HasObserersInfo));
+            control.OnPropertyChanged(nameof(NumberOfObservers));
+        }
+
         public GameInfo GameInfo
         {
             get { return (GameInfo)GetValue(GameInfoProperty); }
@@ -28,5 +38,12 @@ namespace OmegaGo.UI.WindowsUniversal.UserControls.Multiplayer
         public bool HasObserersInfo => (GameInfo is IgsGameInfo);
 
         public int NumberOfObservers => (GameInfo as IgsGameInfo)?.NumberOfObservers ?? 0;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
