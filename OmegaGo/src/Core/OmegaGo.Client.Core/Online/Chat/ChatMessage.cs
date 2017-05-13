952d8e3f71e30abe93a7e2bb8dec81b034eb818e
Petr Hudeček <petrhudecek2010@gmail.com>
2017-05-13 07:07:47 +0200

    change guard

--- OmegaGo/src/Core/OmegaGo.Client.Core/Online/Chat/ChatMessage.cs
+++ OmegaGo/src/Core/OmegaGo.Client.Core/Online/Chat/ChatMessage.cs
@@ -21,10 +21,10 @@ namespace OmegaGo.Core.Online.Chat
         public ChatMessage(string userName, string text, DateTimeOffset time, ChatMessageKind kind)
         {
 #if DEBUG
-            if (String.IsNullOrWhiteSpace(userName))
+            if (userName == null)
                 throw new ArgumentException("Chat senders must have names.", nameof(userName));
             if (text == null)
-                throw new ArgumentException("Chat messages can't be empty.", nameof(text));
+                throw new ArgumentException("Chat messages can't be null.", nameof(text));
 #endif
             this.UserName = userName;
             this.Text = text;
