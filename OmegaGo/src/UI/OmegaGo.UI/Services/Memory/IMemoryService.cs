32f1c3da94a0d5902c3a1ad8d7f55aca9ee76744
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2017-05-14 19:45:11 +0200

    Added new IMemoryService interface and its implementation for UWP.

---
+++ OmegaGo/src/UI/OmegaGo.UI/Services/Memory/IMemoryService.cs
@@ -0,0 +1,15 @@
+﻿namespace OmegaGo.UI.Services.Memory
+{
+    public interface IMemoryService
+    {
+        /// <summary>
+        /// Gets the app's memory usage limit.
+        /// </summary>
+        ulong MemoryUsageLimit { get; }
+
+        /// <summary>
+        /// Gets the app's current memory usage.
+        /// </summary>
+        ulong MemoryUsage { get; }
+    }
+}
---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/Services/Memory/MemoryService.cs
@@ -0,0 +1,12 @@
+﻿using OmegaGo.UI.Services.Memory;
+using Windows.System;
+
+namespace OmegaGo.UI.WindowsUniversal.Services.Memory
+{
+    public sealed class MemoryService : IMemoryService
+    {
+        public ulong MemoryUsageLimit => MemoryManager.AppMemoryUsageLimit;
+
+        public ulong MemoryUsage => MemoryManager.AppMemoryUsage;
+    }
+}
