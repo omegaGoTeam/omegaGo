d24bc899c11a9ab6e30f5081b8a1bdd07152a6a6
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2017-05-15 13:39:46 +0200

    Switch to new HamburgerButton control which handles custom styles.

---
+++ OmegaGo/src/UI/WindowsUniversal/OmegaGo.UI.WindowsUniversal/UserControls/HamburgerButton.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MvvmCross.Platform;
+using OmegaGo.UI.Services.Settings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OmegaGo.UI.WindowsUniversal.UserControls
+{
+    public sealed partial class HamburgerButton : Button
+    {
+        public HamburgerButton()
+        {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            IGameSettings gameSettings = Mvx.Resolve<IGameSettings>();
+
+            if (gameSettings.Display.ControlStyle != Controls.Styles.ControlStyle.OperatingSystem)
+            {
+                HamburgerButtonBorder.Background = null;
+            }
+        }
+    }
+}
