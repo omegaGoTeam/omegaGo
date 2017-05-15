f61ec24e379fbf8d6322b12cb004a6b2fa6423e3
Martin Zikmund <martinzikmund@live.com>
2017-05-15 19:53:54 +0200

    Added Itsukushima Shrine background

--- OmegaGo/src/UI/OmegaGo.UI/Controls/Styles/BackgroundImage.cs
+++ OmegaGo/src/UI/OmegaGo.UI/Controls/Styles/BackgroundImage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OmegaGo.UI.Game.Styles
+﻿namespace OmegaGo.UI.Controls.Styles
 {
     public enum BackgroundImage
     {
@@ -12,6 +6,7 @@ namespace OmegaGo.UI.Game.Styles
         Forest,
         Shrine,
         Temple,
+        ItsukushimaShrine,
         None
     }
 }
